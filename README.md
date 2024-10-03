ed7fe4e998c33a78a2cf46942b8ca3ca59208bf5
Chris Polishak <chrispolishak@gmail.com>
2024-10-03 14:10:41 -0500

    Initial commit - Ailments and History component

--- README.md
+++ README.md
@@ -1,46 +1,34 @@
-# Getting Started with Create React App
+# Self Check in app
+A quickly built self check in app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Things it would need to have
 
-## Available Scripts
+### select patient (if haven't before now)
+- or checking in for patient (ex: a kid)
 
-In the project directory, you can run:
+### Current info
+- Address
+- Email
+- Emergency Contacts
 
-### `npm start`
+### verification of insurance page
+- select insurance on file
+- or enter new insurance
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### patient history 
+- do you have any of the following conditions
+- do you have a family history of these conditions
+   - which family member if so
+- 
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### selecting pharmacy
+- choose on file or add new
+- probably needs maps integration (googleAPI)
 
-### `npm test`
+### summary page - confirmation of details 
+- have list of main details
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+### Testing for all kinds of stuff
+- Need to test components to ensure functioning properly
+- Would need validations probably
+- and more... (to be dealt with later)
---
+++ src/History.tsx
@@ -0,0 +1,211 @@
+import React, { ChangeEvent } from "react";
+import { useState, useEffect } from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { Box, Card, Typography, Button, TextField } from "@mui/material";
+
+const History = () => {
+  interface User {
+    username: string;
+    author: string;
+  }
+
+  interface Faker {
+    data: User[];
+  }
+
+  const [fetchedData, setFetchedData] = useState<User[]>([]);
+  const [ailmentsData, setAilmentsData] = useState<User[]>([]);
+  const [isCheckedAilments, setIsCheckedAilments] = useState<{ [key: string]: boolean }>({});
+  const [isCheckedFamily, setIsCheckedFamily] = useState<{
+    [key: string]: boolean;
+  }>({});
+  const [textInputs, setTextInputs] = useState<{ [key: string]: string }>({});
+
+  // Lets assume this is first time for patient
+  // so we would need to get all data with all unchecked
+  const getAilments = async () => {
+    const url = `https://fakerapi.it/api/v2/books?_quantity=10`;
+    try {
+      const response = await fetch(url);
+      const data: Faker = await response.json();
+      console.log(data.data);
+      setAilmentsData(data.data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const getHistory = async () => {
+    // Fetching names instead of conditions, but the concept is the same
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    try {
+      const response = await fetch(url);
+      const data: User[] = await response.json();
+      // console.log(data);
+      setFetchedData(data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    username: string
+  ) => {
+    setTextInputs({
+      ...textInputs,
+      [username]: e.target.value,
+    });
+  };
+
+  const handleCheckbox = (
+    event: React.SyntheticEvent<Element, Event>,
+    checked: boolean,
+    author: string
+  ) => {
+    setIsCheckedAilments({
+      ...isCheckedAilments,
+      [author]: checked,
+    });
+  };
+
+  const handleCheckboxFamily = (
+    event: React.SyntheticEvent<Element, Event>,
+    checked: boolean,
+    username: string
+  ) => {
+    setIsCheckedFamily({
+      ...isCheckedFamily,
+      [username]: checked,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const checkedAilments = Object.keys(isCheckedAilments)
+    .filter((name) => isCheckedAilments[name])
+    .map((name) => ({
+      issue: name,
+    }));
+
+    const checkedItemsFamily = Object.keys(isCheckedFamily)
+      .filter((name) => isCheckedFamily[name])
+      .map((name) => ({
+        issue: name,
+        familyMember: textInputs[name] || "",
+      }));
+    
+    console.log("Submitted the following Ailments: ", checkedAilments);
+    console.log("Submitted the following Family info: ", checkedItemsFamily);
+    // Ideally would submit via an API call to pass along the necessary data
+  };
+
+  const list = (data: User[]) => {
+    return (
+      // Would need to be in a form to submit user's selected items
+      <form onSubmit={handleSubmit}>
+        <FormGroup sx={{ pb: 2 }}>
+          {data.map((item) => (
+            <FormControlLabel
+              key={item.author}
+              control={
+                <Checkbox
+                  checked={isCheckedAilments[item.author] || false}
+                  onChange={(event, checked) =>
+                    handleCheckbox(event, checked, item.author)
+                  }
+                />
+              }
+              label={item.author}
+            />
+          ))}
+        </FormGroup>
+        <Button type="submit" variant="contained">
+          Save
+        </Button>
+      </form>
+    );
+  };
+
+  const familyList = (data: User[]) => {
+
+    const isAnyTextFieldEmpty = () => {
+      return Object.keys(isCheckedFamily).some(
+        (username) => isCheckedFamily[username] && (!textInputs[username] || textInputs[username].length < 1)
+      );
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <FormGroup sx={{ pb: 2 }}>
+          {data.map((item) => (
+            <div
+              key={item.username}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                paddingBottom: "15px",
+              }}
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={isCheckedFamily[item.username] || false}
+                    onChange={(event, checked) =>
+                      handleCheckboxFamily(event, checked, item.username)
+                    }
+                  />
+                }
+                label={item.username}
+              />
+              {isCheckedFamily[item.username] ? (
+                <TextField
+                  error={textInputs[item.username] === ''}
+                  sx={{ padding: 0 }}
+                  onChange={(e) => handleChange(e, item.username)}
+                  value={textInputs[item.username] || ""}
+                  label="Required"
+                ></TextField>
+              ) : (
+                ""
+              )}
+            </div>
+          ))}
+        </FormGroup>
+        <Button type="submit" disabled={isAnyTextFieldEmpty()} variant="contained">
+          Save
+        </Button>
+      </form>
+    );
+  };
+
+  useEffect(() => {
+    getAilments();
+    getHistory();
+  }, []);
+
+  return (
+    <Box p={4}>
+      <Typography variant="h2">Ailments and Family History</Typography>
+      <hr />
+      <Typography variant="h5" pt={4}>
+        Do you have (or have you had) any of the following issues: (Yes, these
+        are names instead of conditions)
+      </Typography>
+      <Card sx={{ p: 2, backgroundColor: "lightgrey", m: 2, my: 4 }}>
+        {list(ailmentsData)}
+      </Card>
+      <Typography variant="h5">
+        Do you have a family history of any of the following?:
+      </Typography>
+      <Card sx={{ p: 2, backgroundColor: "lightgrey", m: 2, my: 4 }}>
+        {familyList(fetchedData)}
+      </Card>
+    </Box>
+  );
+};
+
+export default History;
